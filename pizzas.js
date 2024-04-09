26c758ca0039fa5866b3d7b9015c94d18c935c22
Christian Ferreira <christian.ferreira7146@hotmail.com>
2024-04-08 21:12:54 -0300

    Criando um site de compra de pizzas

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    { id: 1, name: 'Marguerita', img: 'images/pizza.png', price: 26.90, sizes: ['100g', '530g', '860g'], description: 'Queijo da casa coberto com tomates fatiados com um toque de manjericão.' },
+    { id: 2, name: 'Calabresa', img: 'images/pizza2.png', price: 18.90, sizes: ['320g', '530g', '860g'], description: 'Fatias de calabresa acompanhadas de cebola, azeitonas verdes e queijo.' },
+    { id: 3, name: 'Brasileira', img: 'images/pizza3.png', price: 17.90, sizes: ['320g', '530g', '860g'], description: 'Molho de tomate, queijo, requeijão, presunto e azeitonas verdes.' },
+    { id: 4, name: 'Americana', img: 'images/pizza4.png', price: 19.90, sizes: ['320g', '530g', '860g'], description: 'Molho de tomate, queijo mussarela ralado, ovos cozidos e bacon em fatias.' },
+    { id: 5, name: 'Baiana', img: 'images/pizza5.png', price: 21.90, sizes: ['320g', '530g', '860g'], description:'Muçarela, calabresa fatiada, ovo, bacon, pimenta, cebola.' },
+    { id: 6, name: 'Vegetariana', img: 'images/pizza6.png', price: 18.90, sizes: ['320g', '530g', '860g'], description: 'Molho de tomate, queijo, tomate, champignon, pimentão verde, cebola e azeitonas verdes.' },
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.90, sizes: ['320g', '530g', '860g'], description: 'Chocolate ao leite, chocolate granulado e confete.' }
+];
